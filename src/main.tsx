fef595c854331bd57d532fdf5ea13e94631a4004
DigitalDreamsKC <mary@digitaldreamskc.com>
2025-05-14 18:33:44 -0500

    fix: correct App import in main.tsx for TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import { WalletProviderWrapper } from './WalletProvider'
 import '@solana/wallet-adapter-react-ui/styles.css'
 
-const root = createRoot(document.getElementById('root')!)
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WalletProviderWrapper>
-      <App />
-    </WalletProviderWrapper>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
